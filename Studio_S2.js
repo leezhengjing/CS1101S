ee3715413e6193d1416ae080eeeb62d22e412a1f
Source Academy User <No public email provided>
2022-08-15 14:31:07 +0800

    Studio S2 Worksheet

---
+++ Studio_S2.js
@@ -0,0 +1,33 @@
+function biggie_function(combo) {
+    return combo <= 4 ? combo + 4 : combo;
+}
+
+function unbiggie_function(combo) {
+    return combo >= 5 ? combo - 4 : combo;
+}
+
+function is_biggie_size(combo) {
+    return combo > 4;
+}
+
+function combo_price(combo) {
+    return combo <= 4 ? combo * 1.17 : combo * 1.17 + 0.50;
+}
+
+function empty_order() {
+    return 0;
+}
+
+function add_to_order(order, combo) {
+    return order * 10 + combo;
+}
+
+function last_order(order) {
+    return order % 10;
+}
+
+function other_combos(order) {
+    return (order - last_order(order)) / 10;
+}
+
+other_combos(33);
