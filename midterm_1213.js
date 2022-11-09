7c8038aa466f708a8bbac8ff966a451f33e8d812
Source Academy User <No public email provided>
2022-11-10 02:37:16 +0800

    Changes made from Source Academy

--- midterm_1213.js
+++ midterm_1213.js
@@ -83,5 +83,77 @@ function sum_of_list(xs) {
             ? accum
             : helper(tail(xs), head(xs) + accum);
     }
-    return helper(xs, accum);
+    return helper(xs, 0);
 }
+
+// Task 5c
+function multi_map(f, xxs) {
+    if (is_null(head(xxs))) {
+        return null;
+    } else {
+        return pair(f(map(head,xxs)), multi_map(f, map(tail, xxs)));
+    }
+}
+
+// Task 6A Sudoku
+function make_coordinates(row, column) {
+    return pair(row, column);
+}
+
+function get_x(coordinates) {
+    return head(coordinates);
+}
+
+function get_y(coordinates) {
+    return tail(coordinates);
+}
+
+// Task 6B
+function access(coordinates_list, grid) {
+    return list_ref(list_ref(grid, get_x(coordinates_list)), get_y(coordinates_list));
+}
+
+// Task 6C
+// Theta(n)
+
+// Task 6D
+function all_different(xs) {
+    return is_null(xs)
+        ? true
+        : is_null(member(head(xs), tail(xs))) && all_different(tail(xs));
+}
+
+all_different(list(1, 1, 2, 3));
+
+// Task 6E
+// Theta(n^2)
+
+// Task 6F
+function make_row_coordinates_list(row) {
+    return map(x=> pair(row, x), enum_list(0, 8));
+    // returrn build_list(9, col => make_coordinates(row, col));
+    
+}
+// display_list(make_row_coordinates_list(4));
+
+// Task 6G
+function test_coordinates_list(grid, coordinates_list) {
+    const grid_values_list = map(x => access(x, grid) ,coordinates_list);
+    return all_different(grid_values_list);
+}
+
+// task 6H
+function test_sudoku(grid) {
+    const checklist = make_sudoku_coordinates_list_list();
+    return accumulate(
+                (coordinates_list, sofar) =>
+                    sofar &&
+                    test_coordinates_list(grid, coordinates_list),
+                true,
+                checklist);
+    
+}
+
+// Task 6I
+// This question does not make sense. All Sudoku grids have the same size. 
+// There is no parameter that grows and thus there is no order of growth.
