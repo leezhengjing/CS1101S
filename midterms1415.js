05560cee1c7862538f31d911e0274013f37f4db9
Source Academy User <No public email provided>
2022-11-10 17:15:25 +0800

    Changes made from Source Academy

--- midterms1415.js
+++ midterms1415.js
@@ -1,1 +1,10 @@
 // Midterms 1415
+// Question 1: Making Logical Sense
+// 1A
+// cond_a ? do_y() : do_x()
+// 1B
+// cond_a ? do_x() 
+//        : cond_b ? do_x() : do_y()
+// 1C 
+// cond_a ? (cond_b ? do_x() : do_y())
+//        : do_y() 
