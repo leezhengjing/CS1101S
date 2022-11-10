de1013ee6dd74a25ed58004dec9335e6fee4d2f3
Source Academy User <No public email provided>
2022-11-10 14:52:50 +0800

    Changes made from Source Academy

--- midterm_1314.js
+++ midterm_1314.js
@@ -85,3 +85,51 @@ function zip(f, xs, ys) {
 }
 
 // Question 6 : An accumulating filter
+function filter1(pred, xs) {
+    return accumulate((a, accum_list) => pred(a) ? pair(a, accum_list) : accum_list,
+                       null,
+                       xs);
+}
+
+filter1(x => x >= 1, list(1, 2, 3));
+
+// Create a helper function that stores the accumulated list as an argument then
+// return the accumulated list when the xs is null.
+
+// Question 7A
+const make_queen = pair;
+const row = head;
+const column = tail;
+
+// 7B
+function attack_each_other_diagonally(q1, q2) {
+    return math_abs(row(q1) - row(q2)) === math_abs(column(q1) - column(q2));
+}
+
+// 7C
+function attack_any_diagonally(q1, qs) {
+    return accumulate((a, accum) => attack_each_other_diagonally(q1, a) || accum,
+                      false,
+                      qs);
+}
+
+// 7D
+function attack_diagonally(qs) {
+    return is_null(qs)
+        ? false
+        : attack_any_diagonally(head(qs), tail(qs)) || attack_diagonally(tail(qs));
+}
+
+// Order of growth theta n^2
+
+// 7E
+function permutations(xs) {
+    
+}
+function queens(n) {
+    const permutations_list = permutations(map(x => make_queen(1, x), enum_list(1, n)));
+    return permutations_list;
+}
+
+
+// Order of growth is q * n
